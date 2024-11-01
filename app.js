f4961f049532aeb8eff3dae3ca02359aab2b5cb7
sadafshahab12~ <sadafshahab123@yahoo.com>
2024-11-01 22:28:27 +0500

    nav bar js changing

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 let navMenu = document.querySelector(".logo i");
-let navLinks = document.querySelector(".nav-links")
-const changeIcon = ()=>{
-    navMenu.classList.toggle("fa-xmark");
-    if (navLinks.style.left === "0%") {
-        navLinks.style.left = "-100%";
-      } else {
-        navLinks.style.left = "0%";
-      }
-}
-navMenu.addEventListener("click" , changeIcon)
+let navLinks = document.querySelector(".nav-links");
+const changeIcon = () => {
+  navMenu.classList.toggle("fa-xmark");
+  if (navLinks.style.left === "0%") {
+    navLinks.style.left = "-100%";
+  } else {
+    navLinks.style.left = "0%";
+  }
+};
+navMenu.addEventListener("click", changeIcon);
+
+let navItems = document.querySelectorAll(".nav-items");
+navItems.forEach((nav) => {
+  nav.addEventListener("click", () => {
+    navLinks.style.left = "-100%";
+    navMenu.classList.remove("fa-xmark")
+    navMenu.classList.add("fa-bars");
+    console.log("done")
+  });
+});
